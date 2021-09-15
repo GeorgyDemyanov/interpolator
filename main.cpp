c547d0b60ffca5853e9b0ba0907c87e4c8e935d3
GeorgyDemyanov <72604445+GeorgyDemyanov@users.noreply.github.com>
2021-09-15 23:47:35 +0300

    ШАБЛОНЫ исправлены

--- main.cpp
+++ main.cpp
@@ -4,26 +4,26 @@
 #include <cmath>
 #include "algorithm"
 #include <cassert>
-
-std::vector<double> generateData(int n) {
-    std::vector<double> test(n);
+template <typename T>
+std::vector<T> generateData(int n) {
+    std::vector<T> test(n);
     for (auto &element: test) {
         element = std::sin(rand()) * 1000;
     }
     return test;
 }
-
+template <typename T>
 void test(int n) {
-    std::vector<double> x = generateData(10);
-    std::vector<double> y = generateData(10);
+    std::vector<T> x = generateData<T>(10);
+    std::vector<T> y = generateData<T>(10);
     std::sort(x.begin(), x.end());
     std::sort(y.begin(), y.end());
-    Lagrange<double> lagrange(x, y);
-    Newton<double> newton(x, y);
+    Lagrange<T> lagrange(x, y);
+    Newton<T> newton(x, y);
     for (int i = 0; i < n; i++) {
-        double t = std::sin(rand()) * 0.5 * (x.back() - x[0]) + 0.5 * (x.back() + x[0]);
-        double l = lagrange.interpolate(t);
-        double h = newton.interpolate(t);
+        T t = std::sin(rand()) * 0.5 * (x.back() - x[0]) + 0.5 * (x.back() + x[0]);
+        T l = lagrange.interpolate(t);
+        T h = newton.interpolate(t);
         std::cerr << i << " " << l << " " << h << std::endl;
         assert(std::abs(l-h) <= 1e-8 && "Tb| pidor");
     }
@@ -36,7 +36,7 @@ int main() {
 //    std::cout << "Lagrange: " << otvet.interpolate(1.5) << std::endl;
 //    Newton newton(x, y);
 //    std::cout << "Newton: " << newton.interpolate(1.5) << std::endl;
-    test(100);
+    test<double>(100);
 
     return 0;
 }
